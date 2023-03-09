5c4c08fcd4f4a1d9275e4008e5463e24c7e48c4f
adarsh maharjan <adarsh986030@gmail.com>
2023-03-09 21:56:32 +0545

    [added] default route

--- app.js
+++ app.js
@@ -32,11 +32,14 @@ app.use(
   })
 );
 app.use(helmet());
- 
+
 app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 
